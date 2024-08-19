962a84811dbf8881cd2eb4dbc3abe9e9c70753d4
miladoti2022 <miladoti2021@yahoo.com>
2024-08-19 21:23:17 +0200

    change some functions

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -3,10 +3,9 @@ from fastapi.param_functions import Depends
 from fastapi.security import OAuth2PasswordBearer
 from typing import Optional
 from datetime import datetime, timedelta
-from jose.exceptions import JWTError
-from jose import jwt
-from db.database import get_db
+from jose import jwt, JWTError
 from fastapi import HTTPException, status
+from db.database import get_db
 from db import db_user
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -30,13 +29,13 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail='could not validate credentials',
-        headers={'www-authenticate': 'Bearer'}
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'},
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         user_name: str = payload.get('sub')
-        if user_name in None:
+        if user_name is None:
             raise credentials_exception
     except JWTError:
         raise credentials_exception
@@ -44,4 +43,13 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     user = db_user.get_user_by_username(db, user_name)
     if user is None:
         raise credentials_exception
+
     return user
+
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
